09c65af4b72eed205e7544a0d427a6a7c5a29442
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2022-12-20 01:32:38 +0100

    chore: add custom colors in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customPurple: "#D2C3F2",
+        customBlack: "#111111",
+      },
+    },
   },
   plugins: [],
-}
+};
