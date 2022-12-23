5186f3eba45e36da941ccfe2f04d347414ac503f
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2022-12-23 08:23:44 +0100

    extent tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,14 @@ module.exports = {
       colors: {
         customPurple: "#D2C3F2",
         customBlack: "#111111",
+        customGray: "#434343",
+        customGrayLight: "#F8F8F8",
       },
+      backgroundImage: () => ({
+        "nav-gradient": `linear-gradient(180deg, rgba(255, 255, 255, 0.5) 0%, rgba(255, 255, 255, 0.25) 100%)`,
+        "contact-gradient": `linear-gradient(180deg, #E3FFE9 0%, #F8F8F8 100%)`,
+      }),
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/line-clamp")],
 };
